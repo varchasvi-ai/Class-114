35e148a6078f1ddf60ec0ec39091247692ef5ab9
varchasvi-ai <76240712+varchasvi-ai@users.noreply.github.com>
2021-08-04 18:40:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(500,400);
+    canvas.center();
+    canvas.position(470,300)
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('Image.png');
+}
